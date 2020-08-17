5f2ad890b6bd01a28e97f51bce7bf5b124528b54
gislerio <gisleriojunior@hotmail.com>
2020-08-17 12:20:24 -0300

    inicio do projeto

--- App/Config/Config.php
+++ App/Config/Config.php
@@ -18,8 +18,8 @@ define("DATA_ENV", [
 /* define("MAIL", [
     "host" => "smtp.gmail.com",
     "port" => "587",
-    "user" => "gisleriojunior@gmail.com",
-    "passwd" => "@Gm8714 *",
-    "from_nome"=>"Gislerio Junior",
-    "from_email" => "gisleriojunior@gmail.com"
+    "user" => "email",
+    "passwd" => "senha",
+    "from_nome"=>"nome",
+    "from_email" => "email"
 ]); */
