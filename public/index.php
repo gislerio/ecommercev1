61a43e5aafe69e1d591bdda7d299333df2054270
gislerio <gisleriojunior@hotmail.com>
2020-08-17 23:19:42 -0300

    criando pageadmin e tpl admin

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Config\Page;
+use App\Config\PageAdmin;
 use Slim\Slim;
 
 require_once("../vendor/autoload.php");
@@ -13,7 +14,12 @@ $app->get('/', function() {
     
 	$page = new Page();
 	$page->setTpl("index");
+});
 
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
+	$page->setTpl("index");
 });
 
 $app->run();
